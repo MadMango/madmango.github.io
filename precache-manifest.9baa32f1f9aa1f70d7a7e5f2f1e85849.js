d1eb8f68b7ce350e3a96c8eeb09f9b9366bae444
madmang <msf11235813@gmail.com>
2019-07-28 14:10:02 +0100

    push

---
+++ precache-manifest.9baa32f1f9aa1f70d7a7e5f2f1e85849.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "411e9ab71993d662e1ba0058873c1787",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a05d80e3d8930674d226",
+    "url": "/static/css/main.9dd1aadd.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "a05d80e3d8930674d226",
+    "url": "/static/js/main.22dc0660.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
