cfa8290b00466bdbdfedf01a09e382dcfab5cc53
verymadmango <verymadmango@gmail.com>
2020-03-24 16:36:58 +0000

    update availability

---
+++ build/precache-manifest.015300e91d965cd72dd061a2a71acd13.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b006c7c93d516914f8e9b24658a3a89c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e967cbcdbdd6bda8ba9",
+    "url": "/static/css/main.c68a82f8.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "6e967cbcdbdd6bda8ba9",
+    "url": "/static/js/main.d0e8c2dd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
